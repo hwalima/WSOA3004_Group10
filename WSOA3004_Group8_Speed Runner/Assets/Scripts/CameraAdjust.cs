8c9eb113cc8de819637e9579ef8731024088fd34
hexedboy <64075822+hexedboy@users.noreply.github.com>
2020-09-12 19:40:02 +0200

    Level implementation1

---
+++ WSOA3004_Group8_Speed Runner/Assets/Scripts/CameraAdjust.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraAdjust : MonoBehaviour
+{
+    public CinemachineVirtualCamera cam;
+    public float camSize;
+    public float prevCamSize;
+    public float speed;
+    public bool lerpCam = false;
+    private bool prevCam = false;
+    private float t = 0f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (lerpCam == true && prevCam == false) //if the player goes forward, it'll swap to the camera size for the next area
+        {
+            t += speed * Time.deltaTime;
+            cam.m_Lens.OrthographicSize = Mathf.Lerp(cam.m_Lens.OrthographicSize, camSize, speed * Time.deltaTime);
+            if (t >= 1)
+            {
+                lerpCam = false;
+                t = 0;
+            }
+        }
+        else if (lerpCam == true && prevCam == true) //if the player goes backwards, it'll swap back to the camera size of the previous area
+        {
+            t += speed * Time.deltaTime;
+            cam.m_Lens.OrthographicSize = Mathf.Lerp(cam.m_Lens.OrthographicSize, prevCamSize, speed * Time.deltaTime);
+            if (t >= 1)
+            {
+                lerpCam = false;
+                t = 0;
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (collision.GetComponent<Rigidbody2D>().velocity.x > 0) //check if player moving right
+            {
+                lerpCam = true;
+                prevCam = false;
+                //cam.m_Lens.OrthographicSize = camSize;
+            }
+            else if (collision.GetComponent<Rigidbody2D>().velocity.x < 0) //check if player moving left
+            {
+                lerpCam = true;
+                prevCam = true;
+                //cam.m_Lens.OrthographicSize = prevCamSize;
+                
+            }
+            
+        }
+        
+    }
+
+    
+   
+}
---
+++ WSOA3004_Group8_Speed Runner/Assets/Scripts/PulseController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseController : MonoBehaviour
+{
+
+    //stuff to make the lillies bounce
+    public Vector3 startPt; //starting size of the lillypad, you can set in the inspector
+    public Vector3 EndPt; //ending size of the lillypad, same as above
+    private float animationTimePosition;
+    public float duration = 0.5f; //both speed and duration effects the bounce speed of the lillypads, idk i'm tired so just tweak them to get a feel
+    public float speed = 2f;
+
+    //particle stuff
+
+
+    IEnumerator Start() //changing from void to numerator allows the coroutine to loop
+    {
+        while (true)
+        {
+            //grow lillypad
+            yield return BounceLilly(startPt, EndPt, duration);
+            //shrink lillypad
+            yield return BounceLilly(EndPt, startPt, duration);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+
+    private IEnumerator BounceLilly(Vector3 a, Vector3 b, float time)
+    {
+        float i = 0.0f; //zero is the start point
+        float rate = (1.0f / time) * speed; //one is the end point, and over time rate is being multiplied by time and by the speed we set in the inspector
+
+        while (i < 1.0f) //goes until it reaches one??
+        {
+            i += Time.deltaTime * rate; //just your typical lerping stuff
+            transform.localScale = Vector3.Lerp(a, b, i);
+            yield return null; //needs to just return something or the numerator will be angry >:~(
+        }
+    }
+}
+
