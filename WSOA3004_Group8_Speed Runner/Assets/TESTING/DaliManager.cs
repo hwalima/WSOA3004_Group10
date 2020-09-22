7cc67627b1f434ad2ad0a84c57ff9ddaef2f96ae
leodalessss <53822762+leodalessss@users.noreply.github.com>
2020-09-22 12:24:30 +0200

    LEOS SHIT IS FUCKED
    asdasdas

---
+++ WSOA3004_Group8_Speed Runner/Assets/TESTING/DaliManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaliManager : MonoBehaviour
+{
+    public GameObject DaliFam1;
+    //public GameObject DaliFam1Ativator;
+    bool daliFam1Activated=false;
+
+
+    public GameObject DaliFam2;
+    bool daliFam2Activated = false;
+
+    public GameObject DaliFam3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!daliFam1Activated)
+
+        {
+            Vector2 f1p1 = new Vector2(168.22f, 47f);
+            Vector2 f1p2 = new Vector2(172.9f, 41.98f);
+            Collider2D[] findPlayer = Physics2D.OverlapAreaAll(f1p1, f1p2);
+            if (findPlayer != null)
+            {
+                for (int i = 0; i < findPlayer.Length; i++)
+                {
+                   if(findPlayer[i].gameObject.tag == "Player")
+                    {
+                        DaliFam1.SetActive(true);
+                        daliFam1Activated = true;
+                    }
+                }
+            }
+        }
+
+
+        if (!daliFam2Activated)
+        {
+            Vector2 f1p1 = new Vector2(193.9f,0.8f);
+            Vector2 f1p2 = new Vector2(197.9f,15.7f);
+            Collider2D[] findPlayer = Physics2D.OverlapAreaAll(f1p1, f1p2);
+          
+            if (findPlayer != null)
+            {
+                for (int i = 0; i < findPlayer.Length; i++)
+                {
+                    if (findPlayer[i].gameObject.tag == "Player")
+                    {
+                        DaliFam2.SetActive(true);
+                        daliFam2Activated = true;
+                        StartCoroutine(SecondFamilySet());
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator SecondFamilySet()
+    {
+        daliFam2Activated = true;
+        yield return new WaitForSeconds(0.5f);
+        DaliFam3.SetActive(true);
+    }
+}
---
+++ WSOA3004_Group8_Speed Runner/Assets/TESTING/GeneralDaliManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GeneralDaliManager : MonoBehaviour
+{
+    public GameObject babyDali;
+    public GameObject momDali;
+    public GameObject dadDali;
+    
+
+
+    int attackNo = 0;
+
+    List<DaliHeadControl> orderOfAttack = new List<DaliHeadControl>();
+
+    public bool next=false;
+
+    bool beginAttacking = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!beginAttacking)
+        {
+            if (babyDali.GetComponent<DaliHeadControl>().isAttacking==true)
+            {
+               // Debug.Log("here");
+                orderOfAttack.Add(babyDali.GetComponent<DaliHeadControl>());
+                orderOfAttack.Add(momDali.GetComponent<DaliHeadControl>());
+                orderOfAttack.Add(dadDali.GetComponent<DaliHeadControl>());
+                beginAttacking = true;
+            }
+            else if (momDali.GetComponent<DaliHeadControl>().isAttacking==true)
+            {                
+                orderOfAttack.Add(momDali.GetComponent<DaliHeadControl>());
+                orderOfAttack.Add(babyDali.GetComponent<DaliHeadControl>());
+                orderOfAttack.Add(dadDali.GetComponent<DaliHeadControl>());
+                beginAttacking = true;
+            }
+            else if (dadDali.GetComponent<DaliHeadControl>().isAttacking==true)
+            {
+                orderOfAttack.Add(dadDali.GetComponent<DaliHeadControl>());
+                orderOfAttack.Add(babyDali.GetComponent<DaliHeadControl>());
+                orderOfAttack.Add(momDali.GetComponent<DaliHeadControl>());
+                beginAttacking = true;
+            }
+            
+
+        }
+
+        if (orderOfAttack.Count > 1)
+        {
+            
+            if (next)
+            {
+                orderOfAttack.Remove(orderOfAttack[attackNo]);
+                Debug.Log(orderOfAttack.Count +"shuyfbweuf" );
+                orderOfAttack[attackNo].FindPlayer();
+                next = false;
+               
+            }
+            if (!next)
+            {
+                for (int i = 0; i < orderOfAttack.Count; i++)
+                {
+                   // Debug.Log(i + " i" + orderOfAttack.Count + " order of attack"+orderOfAttack.Count);
+                //    Debug.Log(orderOfAttack.Count + "count number " + i + " i");
+                    if (i == attackNo)
+                    {
+                        orderOfAttack[i].isAttacking = true;
+                        orderOfAttack[i].waitBeforeAttacking = false;
+                    }
+                    else
+                    {
+                        orderOfAttack[i].isAttacking = false;
+                        orderOfAttack[i].waitBeforeAttacking = true;
+                    }
+
+                }
+            }
+
+           
+        }
+
+        if (orderOfAttack.Count == 0)
+        {           
+            if (beginAttacking == true)
+            {
+               // Debug.Log("there are " + orderOfAttack.Count + " objects left");
+                Destroy(gameObject);
+            }
+        }
+
+        
+    }
+}
