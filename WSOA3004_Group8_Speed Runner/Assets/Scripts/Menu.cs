e7d8ca99d675de584a5c2d2afd8b8b0af81da577
hwalima <hwalima@outlook.com>
2020-11-22 13:23:41 +0200

    sound 2

---
+++ WSOA3004_Group8_Speed Runner/Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public void playonClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
+
+    
